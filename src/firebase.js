ab023c574180fb866772b2bbe1a525c3ed6646df
Patrick Dolan <dolanp1992@gmail.com>
2023-01-16 18:28:26 -0800

    docs: remove comment that links to firebase sdks

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
