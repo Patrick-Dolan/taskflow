74146fa430c63d1586d4f70a3976b728b4f47e31
Patrick Dolan <dolanp1992@gmail.com>
2023-01-17 15:53:51 -0800

    update: add email password reset link system

--- src/Components/tempAuth/AccountOptions.js
+++ src/Components/tempAuth/AccountOptions.js
@@ -2,19 +2,18 @@ import { UserAuth } from "../../Contexts/AuthContext";
 import { useState } from "react";
 
 const AccountOptions = () => {
-  const { signIn, logout } = UserAuth();
+  const { signIn, logout, passwordResetEmail } = UserAuth();
   const [email, setEmail] = useState("");
+  const [passwordEmail, setPasswordEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSignIn = async (e) => {
     e.preventDefault();
 
     try {
       await signIn(email, password);
     } catch (e) {
-      setError(e.message);
-      console.log("Sign in User Error: ", error);
+      console.log(e.message);
     }
   }
 
@@ -26,11 +25,22 @@ const AccountOptions = () => {
     }
   }
 
+  const handlePasswordReset = async (e) => {
+    e.preventDefault();
+
+    try {
+      await passwordResetEmail(passwordEmail);
+      console.log("Password Reset Email Sent");
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
   return (
     <div>
       <div>
         <h3>Sign in</h3>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSignIn}>
           <label>Email</label>
           <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
           <label>Password</label>
@@ -41,6 +51,14 @@ const AccountOptions = () => {
       <div>
         <h3>Logout</h3>
         <button onClick={handleLogout}>Logout</button>
+      </div>
+      <div>
+        <h3>Password Reset</h3>
+        <form onSubmit={handlePasswordReset}>
+          <label>Email</label>
+          <input type="email" name="email" onChange={(e) => setPasswordEmail(e.target.value)} />
+          <button type="submit">Send Reset Email</button>
+        </form>
       </div>
     </div>
   )
