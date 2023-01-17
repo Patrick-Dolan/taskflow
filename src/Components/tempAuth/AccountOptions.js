26c204988fb1b8771934897e6284ee38a94ad8d6
Patrick Dolan <dolanp1992@gmail.com>
2023-01-17 15:38:04 -0800

    update: add signin functionality to temp account options component

--- src/Components/tempAuth/AccountOptions.js
+++ src/Components/tempAuth/AccountOptions.js
@@ -1,7 +1,23 @@
 import { UserAuth } from "../../Contexts/AuthContext";
+import { useState } from "react";
 
 const AccountOptions = () => {
-  const { logout } = UserAuth();
+  const { signIn, logout } = UserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signIn(email, password);
+    } catch (e) {
+      setError(e.message);
+      console.log("Sign in User Error: ", error);
+    }
+  }
+
   const handleLogout = async () => {
     try {
       await logout();
@@ -12,8 +28,20 @@ const AccountOptions = () => {
 
   return (
     <div>
-      {/* <button onClick={handleLogin}>Login</button> */}
-      <button onClick={handleLogout}>Logout</button>
+      <div>
+        <h3>Sign in</h3>
+        <form onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
+          <label>Password</label>
+          <input type="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      <div>
+        <h3>Logout</h3>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </div>
   )
 }
