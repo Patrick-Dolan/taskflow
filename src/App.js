09d2da0c7533d9cc14b833bd71930bd548eedf7e
Patrick Dolan <dolanp1992@gmail.com>
2023-01-17 18:14:16 -0800

    update: add responsive appbar and routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { AuthProvider } from "./Contexts/AuthContext";
-import RegisterUser from "./Components/tempAuth/RegisterUser";
-import AccountOptions from "./Components/tempAuth/AccountOptions";
+import { Routes, Route } from "react-router";
+import ResponsiveAppBar from "./Components/nav/ResponsiveAppBar";
+import Homepage from "./Components/home/Homepage";
+import Projects from "./Components/projects/Projects";
+import Tasks from "./Components/tasks/Tasks";
+import Settings from "./Components/settings/Settings";
+import Account from "./Components/account/Account";
+import Profile from "./Components/account/Profile";
 
 const App = () => {
   return (
     <AuthProvider>
-      <h1>TaskFlow</h1>
-      <RegisterUser />
-      <AccountOptions />
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="Home" element={<Homepage />} />
+        <Route path="Projects" element={<Projects />} />
+        <Route path="Tasks" element={<Tasks />} />
+        <Route path="Profile" element={<Profile />} />
+        <Route path="Account" element={<Account />} />
+        <Route path="Settings" element={<Settings />} />
+      </Routes>
     </AuthProvider>
   );
 }
