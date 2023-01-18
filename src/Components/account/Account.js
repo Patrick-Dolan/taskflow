2088e7edb8988ba22184f4ac6e003dc07d5e3472
Patrick Dolan <dolanp1992@gmail.com>
2023-01-17 18:13:25 -0800

    docs: add comments to make it clear what is temporary

--- src/Components/account/Account.js
+++ src/Components/account/Account.js
@@ -1,6 +1,7 @@
 import { Container } from "@mui/system";
 import { Typography } from "@mui/material";
 
+// Temp Components
 import RegisterUser from "../tempAuth/RegisterUser";
 import AccountOptions from "../tempAuth/AccountOptions";
 
@@ -8,8 +9,10 @@ const Account = () => {
   return (
     <Container maxWidth="md">
       <Typography variant="h4">Account placeholder</Typography>
+      {/* Temp Components */}
       <RegisterUser />
       <AccountOptions />
+      {/* --------------- */}
     </Container>
   )
 }
