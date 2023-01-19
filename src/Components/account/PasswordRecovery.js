45c62ade3d0cc5475e4e64247468f26e11d9d65b
Patrick Dolan <dolanp1992@gmail.com>
2023-01-18 17:10:28 -0800

    feature: add password recovery page component placeholder

---
+++ src/Components/account/PasswordRecovery.js
@@ -0,0 +1,12 @@
+import { Container } from "@mui/system";
+import { Typography } from "@mui/material";
+
+const PasswordRecovery = () => {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4">Password Recovery placeholder</Typography>
+    </Container>
+  )
+}
+
+export default PasswordRecovery;
