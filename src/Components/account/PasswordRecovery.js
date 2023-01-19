f62063ab67af8ce3a6c06aff3e42f7ffd781f359
Patrick Dolan <dolanp1992@gmail.com>
2023-01-18 18:09:03 -0800

    update: setup password recovery form and functionality with userauth

--- src/Components/account/PasswordRecovery.js
+++ src/Components/account/PasswordRecovery.js
@@ -1,10 +1,44 @@
 import { Container } from "@mui/system";
 import { Typography } from "@mui/material";
+import { UserAuth } from "../../Contexts/AuthContext";
+import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import { Box } from "@mui/system";
+import { Button } from "@mui/material";
+
 
 const PasswordRecovery = () => {
+  const { passwordResetEmail } = UserAuth();
+  const [passwordEmail, setPasswordEmail] = useState("");
+
+
+  const handlePasswordReset = async (e) => {
+    e.preventDefault();
+
+    try {
+      await passwordResetEmail(passwordEmail);
+      console.log("Password Reset Email Sent");
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
   return (
     <Container maxWidth="md">
-      <Typography variant="h4">Password Recovery placeholder</Typography>
+      <Box>
+        <Typography variant="h4">Password Recovery placeholder</Typography>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="outlined"
+          required
+          onChange={(e) => setPasswordEmail(e.target.value)}
+        />
+        <Button fullWidth size="large" onClick={handlePasswordReset} variant="contained">Send password reset email</Button>
+      </Box>
     </Container>
   )
 }
