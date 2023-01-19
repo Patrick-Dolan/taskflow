2e2471be25fcc4cd06b35794462f2092152ee758
Patrick Dolan <dolanp1992@gmail.com>
2023-01-19 11:04:24 -0800

    update: add theme provider to use theme throughtout project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { AuthProvider } from "./Contexts/AuthContext";
 import { Routes, Route } from "react-router";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./Components/theme";
 import ResponsiveAppBar from "./Components/nav/ResponsiveAppBar";
 import Homepage from "./Components/home/Homepage";
 import Projects from "./Components/projects/Projects";
@@ -12,17 +14,19 @@ import PasswordRecovery from "./Components/account/PasswordRecovery";
 const App = () => {
   return (
     <AuthProvider>
-      <ResponsiveAppBar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="Home" element={<Homepage />} />
-        <Route path="Projects" element={<Projects />} />
-        <Route path="Tasks" element={<Tasks />} />
-        <Route path="Profile" element={<Profile />} />
-        <Route path="Account" element={<Account />} />
-        <Route path="Settings" element={<Settings />} />
-        <Route path="PasswordRecovery" element={<PasswordRecovery />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="Home" element={<Homepage />} />
+          <Route path="Projects" element={<Projects />} />
+          <Route path="Tasks" element={<Tasks />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="Account" element={<Account />} />
+          <Route path="Settings" element={<Settings />} />
+          <Route path="PasswordRecovery" element={<PasswordRecovery />} />
+        </Routes>
+      </ThemeProvider>
     </AuthProvider>
   );
 }
