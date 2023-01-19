0a9e85575a19e1bd1e9cfcff366740249a243605
Patrick Dolan <dolanp1992@gmail.com>
2023-01-18 17:25:47 -0800

    update: remove comment and change base state of user to null

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -5,10 +5,9 @@ import { auth } from "../firebase"
 export const UserContext = createContext();
 
 export const AuthProvider = ({children}) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   
-  // !!! LEAVE FOLLOWING CONSOLE LOG IN BUT COMMENT OUT BEFORE PUSHING CODE !!!
-  // console.log("Current User: ", user)
+  console.log("Current User: ", user)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
