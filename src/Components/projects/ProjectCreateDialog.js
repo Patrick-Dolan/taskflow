abacaee2ac51c8daf9721ed0b27e79c915fa1f3e
Patrick Dolan <dolanp1992@gmail.com>
2023-01-19 22:11:40 -0800

    feature: add project create dialog and add it to project page component

---
+++ src/Components/projects/ProjectCreateDialog.js
@@ -0,0 +1,83 @@
+import { Container } from '@mui/system';
+import { forwardRef } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import { TextField } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const ProjectCreateDialog = (props) => {
+  const { open, setOpen, setProjectName, setProjectDescription, createProject } = props; 
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCreateProject = () => {
+    createProject();
+    setOpen(false);
+  }
+
+  return (
+    <div>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Create new project
+            </Typography>
+            {/* TODO decide if I will use save button and its functionality */}
+            {/* <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button> */}
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="md">
+          <TextField
+            type="text"
+            label="Project Name"
+            onChange={(e) => setProjectName(e.target.value)}
+            margin="normal"
+            fullWidth
+            required
+          />
+          <TextField
+            type="text"
+            label="Description"
+            onChange={(e) => setProjectDescription(e.target.value)}
+            size="small"
+            minRows="4"
+            margin="normal"
+            multiline
+            fullWidth
+            required
+          />
+          <Button variant="outlined" onClick={handleCreateProject}>Create Project SUBMIT</Button>
+        </Container>
+      </Dialog>
+    </div>
+  );
+}
+
+export default ProjectCreateDialog;
--- src/Components/projects/Projects.js
+++ src/Components/projects/Projects.js
@@ -1,10 +1,42 @@
 import { Container } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
+import ProjectCreateDialog from "./ProjectCreateDialog";
 
 const Projects = () => {
+  const [newProject, setNewProject] = useState({});
+
+  // Dialog State
+  const [openProjectCreate, setOpenProjectCreate] = useState(false);
+  
+  // Create project State
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  
+  const handleProjectCreateOpen = () => {
+    setOpenProjectCreate(true);
+  }
+  
+  const handleCreateProject = () => {
+    setNewProject({
+      name: projectName,
+      description: projectDescription
+    })
+  }
+
+  console.log(newProject);
+
   return (
-    <Container maxWidth="md">
-      <Typography variant="h4">Projects placeholder</Typography>
+    <Container maxWidth="md" sx={{marginTop: "1.5em"}}>
+      <Typography variant="h4">Projects</Typography>
+      <Button variant="outlined" onClick={handleProjectCreateOpen}>Create Project</Button>
+      <ProjectCreateDialog
+        open={openProjectCreate}
+        setOpen={setOpenProjectCreate}
+        setProjectName={setProjectName}
+        setProjectDescription={setProjectDescription}
+        createProject={handleCreateProject}
+      />
     </Container>
   )
 }
