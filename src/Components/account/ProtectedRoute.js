f33b16c0a02496235d1128f60ca4edaf5cfb9330
Patrick Dolan <dolanp1992@gmail.com>
2023-01-21 15:03:32 -0800

    fix: add dependencies to dependency array

--- src/Components/account/ProtectedRoute.js
+++ src/Components/account/ProtectedRoute.js
@@ -10,7 +10,7 @@ const ProtectedRoute = (props) => {
     if (!user) {
       handleUnauthorizedAccess();
     }
-  }, [snackbarOpen])
+  }, [snackbarOpen, handleUnauthorizedAccess, user])
 
   if (!user) {
     return <Navigate to="/" />
