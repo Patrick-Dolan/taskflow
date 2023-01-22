643e10da886890559559950f49413090dd174495
Patrick Dolan <dolanp1992@gmail.com>
2023-01-22 14:56:02 -0800

    update: add project edit functionality to details component

--- src/Components/projects/ProjectDetails.js
+++ src/Components/projects/ProjectDetails.js
@@ -1,8 +1,42 @@
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
+import ProjectEditDialog from "./ProjectEditDialog";
 
 const ProjectDetails = (props) => {
-  const { project, setSelectedProject } = props;
+  const { project, projects, setProjects, setSelectedProject } = props;
+
+  // Edit project State
+  const [editedProjectName, setEditedProjectName] = useState("");
+  const [editedProjectDescription, setEditedProjectDescription] = useState("");
+  const [openProjectEdit, setOpenProjectEdit] = useState(false);
+  
+  const handleProjectEditOpen = () => {
+    setOpenProjectEdit(true);
+    setEditedProjectName(project?.name);
+    setEditedProjectDescription(project?.description);
+  }
+
+  const handleUpdateProject = () => {
+    const filteredProjects = projects.filter((p) => p.id !== project.id);
+    
+    // Update ProjectsList component information
+    setProjects([
+      ...filteredProjects,
+      {
+        ...project,
+        name: editedProjectName,
+        description: editedProjectDescription
+      }
+    ])
+
+    // Updates state for details page to show new info
+    setSelectedProject({
+      ...project,
+      name: editedProjectName,
+      description: editedProjectDescription
+    })
+  }
 
   return (
     <>
@@ -14,7 +48,7 @@ const ProjectDetails = (props) => {
       }}
       >
         <Button variant="contained" onClick={() => setSelectedProject({})}>Go Back</Button>
-        <Button variant="contained">Edit</Button>
+        <Button variant="contained" onClick={handleProjectEditOpen}>Edit</Button>
       </Box>
       <Typography variant="caption">Project Name</Typography>
       <Typography variant="h5">{project?.name}</Typography>
@@ -28,7 +62,14 @@ const ProjectDetails = (props) => {
       ) : (
           <Typography>No tasks assigned to this project.</Typography>
       )}
-      
+      <ProjectEditDialog 
+        open={openProjectEdit}
+        setOpen={setOpenProjectEdit}
+        project={project}
+        setEditedProjectName={setEditedProjectName}
+        setEditedProjectDescription={setEditedProjectDescription}
+        updateProject={handleUpdateProject}
+      />
     </>
   )
 }
