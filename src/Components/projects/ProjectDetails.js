2edd4ced690a76eea96de967e7e54634a173f592
Patrick Dolan <dolanp1992@gmail.com>
2023-01-22 22:26:44 -0800

    update: add project delete dialog box

--- src/Components/projects/ProjectDetails.js
+++ src/Components/projects/ProjectDetails.js
@@ -2,13 +2,19 @@ import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
 import ProjectEditDialog from "./ProjectEditDialog";
+import ProjectDeleteDialog from "./ProjectDeleteDialog";
 
 const ProjectDetails = (props) => {
   const { project, projects, setProjects, setSelectedProject } = props;
   const [openProjectEdit, setOpenProjectEdit] = useState(false);
+  const [openProjectDelete, setOpenProjectDelete] = useState(false);
   
   const handleProjectEditOpen = () => {
     setOpenProjectEdit(true);
+  }
+
+  const handleProjectDeleteOpen = () => {
+    setOpenProjectDelete(true);
   }
 
   return (
@@ -21,7 +27,10 @@ const ProjectDetails = (props) => {
       }}
       >
         <Button variant="contained" onClick={() => setSelectedProject({})}>Go Back</Button>
-        <Button variant="contained" onClick={handleProjectEditOpen}>Edit</Button>
+        <Box>
+          <Button variant="contained" onClick={handleProjectEditOpen} sx={{marginRight: ".5em"}}>Edit</Button>
+          <Button variant="contained" onClick={handleProjectDeleteOpen}>Delete</Button>
+        </Box>
       </Box>
       <Typography variant="caption">Project Name</Typography>
       <Typography variant="h5">{project?.name}</Typography>
@@ -38,6 +47,14 @@ const ProjectDetails = (props) => {
       <ProjectEditDialog 
         open={openProjectEdit}
         setOpen={setOpenProjectEdit}
+        project={project}
+        projects={projects}
+        setProjects={setProjects}
+        setSelectedProject={setSelectedProject}
+      />
+      <ProjectDeleteDialog 
+        open={openProjectDelete}
+        setOpen={setOpenProjectDelete}
         project={project}
         projects={projects}
         setProjects={setProjects}
