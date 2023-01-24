4ca6699e57fc7c14e3cda2eabec64296feeeccbc
Patrick Dolan <dolanp1992@gmail.com>
2023-01-24 08:59:20 -0800

    update: add colors to palette

--- src/Components/theme/index.js
+++ src/Components/theme/index.js
@@ -5,16 +5,22 @@ export const theme = createTheme({
     type: 'light',
     primary: {
       main: '#0c0c0c',
+      light: '#3C3C3C',
+      dark: '#080808'
     },
     secondary: {
       main: '#1c64bc',
-      light: '#4983C9'
+      light: '#4983C9',
+      dark: '#134683',
+      contrastText: '#fff'
     },
     error: {
       main: '#ff002d',
     },
     success: {
       main: '#26c72c',
+      light: '#51D256',
+      dark: '#1A8B1E'
     },
   }
 })
