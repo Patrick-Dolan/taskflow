6ff333bd29941983ac3fab5fa7d8442a8161c4bf
Patrick Dolan <dolanp1992@gmail.com>
2023-01-24 02:15:28 -0800

    feature: add TaskDetails component

---
+++ src/Components/tasks/TaskDetails.js
@@ -0,0 +1,36 @@
+import { Box } from "@mui/system";
+import { Button, Typography } from "@mui/material";
+
+const TaskDetails = (props) => {
+  const { task, setSelectedTask } = props;
+
+  const handleGoBackClick = () => {
+    setSelectedTask({});
+  }
+
+  return (
+    <>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginTop: ".5em",
+        marginBottom: "1em",
+      }}
+      >
+        <Button variant="contained" onClick={() => setSelectedTask({})}>Go Back</Button>
+        <Box>
+          <Button variant="contained" onClick={handleGoBackClick} sx={{marginRight: ".5em"}}>Edit</Button>
+          <Button variant="contained" onClick={handleGoBackClick}>Delete</Button>
+        </Box>
+      </Box>
+      <Typography variant="caption">Task</Typography>
+      <Typography variant="h5">{task?.name}</Typography>
+      <Typography variant="caption">Description</Typography>
+      <Typography>{(task?.description) ? `${task.description}` : "No description available."}</Typography>
+      <Typography variant="caption">Description</Typography>
+      <Typography>{(task?.dueDateAssigned) ? `${task.dueDate.format("MM-DD-YYYY")}` : "No due date assigned."}</Typography>
+    </>
+  )
+}
+
+export default TaskDetails;
