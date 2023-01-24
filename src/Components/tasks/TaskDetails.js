c4aaa3ca758c8509acbb1e0c5c9fa33051e0f240
Patrick Dolan <dolanp1992@gmail.com>
2023-01-24 02:32:48 -0800

    update: add assigned to section and fix caption for due date

--- src/Components/tasks/TaskDetails.js
+++ src/Components/tasks/TaskDetails.js
@@ -27,8 +27,11 @@ const TaskDetails = (props) => {
       <Typography variant="h5">{task?.name}</Typography>
       <Typography variant="caption">Description</Typography>
       <Typography>{(task?.description) ? `${task.description}` : "No description available."}</Typography>
-      <Typography variant="caption">Description</Typography>
+      <Typography variant="caption">Due date</Typography>
       <Typography>{(task?.dueDateAssigned) ? `${task.dueDate.format("MM-DD-YYYY")}` : "No due date assigned."}</Typography>
+      <Typography variant="caption">Assigned to:</Typography>
+      {/* TODO: set up assignment system */}
+      <Typography>{(task?.assignedTo) ? `FEATURE NOT IMPLEMENTED YET` : "FEATURE NOT IMPLEMENTED YET"}</Typography>
     </>
   )
 }
