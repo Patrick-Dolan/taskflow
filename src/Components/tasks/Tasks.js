a9130a40e312310f2a485fae8d6a0111a0fe9245
Patrick Dolan <dolanp1992@gmail.com>
2023-01-23 18:04:47 -0800

    update: add TaskCreateDialog for task creation     update: pass handleTaskCreateOpen to toolbar for dialog opening

--- src/Components/tasks/Tasks.js
+++ src/Components/tasks/Tasks.js
@@ -1,10 +1,31 @@
 import { Container } from "@mui/system";
 import TasksToolbar from "./TasksToolbar";
+import TaskCreateDialog from "./TaskCreateDialog";
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+  const [openTaskCreate, setOpenTaskCreate] = useState(false);
+
+  const handleTaskCreateOpen = () => {
+    setOpenTaskCreate(true);
+  }
+
+  console.log(tasks)
+
   return (
     <Container maxWidth="md" sx={{marginTop: "1.5em"}}>
-      <TasksToolbar />
+      <TasksToolbar 
+        handleTaskCreateOpen={handleTaskCreateOpen}
+      />
+      <TaskCreateDialog
+        open={openTaskCreate}
+        setOpen={setOpenTaskCreate}
+        tasks={tasks}
+        setTasks={setTasks}
+        handleTaskCreateOpen={handleTaskCreateOpen}
+        // setSelectedProject={setSelectedProject}
+      />
     </Container>
   )
 }
