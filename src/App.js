e14f9d3dd6d9218b91f985f5eaa6373a0e52c412
Patrick Dolan <dolanp1992@gmail.com>
2023-01-23 18:02:31 -0800

    update: add localization provider for mui date picker system

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./Components/theme";
 import { useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import ResponsiveAppBar from "./Components/nav/ResponsiveAppBar";
 import Homepage from "./Components/home/Homepage";
 import Projects from "./Components/projects/Projects";
@@ -23,38 +25,40 @@ const App = () => {
 
   return (
     <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <ResponsiveAppBar snackbarOpen={snackbarOpen} setSnackbarOpen={setSnackbarOpen} />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="Home" element={<Homepage />} />
-          <Route path="Projects" element={
-            <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
-              <Projects />
-            </ProtectedRoute>
-            } 
-          />
-          <Route path="Tasks" element={
-            <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
-              <Tasks />
-            </ProtectedRoute>
-            } 
-          />
-          <Route path="Profile" element={
-            <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
-              <Profile />
-            </ProtectedRoute>
-            } />
-          <Route path="Account" element={
-            <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
-              <Account />
-            </ProtectedRoute>
-            } 
-          />
-          <Route path="Settings" element={<Settings />} />
-          <Route path="PasswordRecovery" element={<PasswordRecovery />} />
-        </Routes>
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ThemeProvider theme={theme}>
+          <ResponsiveAppBar snackbarOpen={snackbarOpen} setSnackbarOpen={setSnackbarOpen} />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="Home" element={<Homepage />} />
+            <Route path="Projects" element={
+              <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
+                <Projects />
+              </ProtectedRoute>
+              }
+            />
+            <Route path="Tasks" element={
+              <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
+                <Tasks />
+              </ProtectedRoute>
+              }
+            />
+            <Route path="Profile" element={
+              <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
+                <Profile />
+              </ProtectedRoute>
+              } />
+            <Route path="Account" element={
+              <ProtectedRoute snackbarOpen={snackbarOpen} handleUnauthorizedAccess={handleUnauthorizedAccess}>
+                <Account />
+              </ProtectedRoute>
+              }
+            />
+            <Route path="Settings" element={<Settings />} />
+            <Route path="PasswordRecovery" element={<PasswordRecovery />} />
+          </Routes>
+        </ThemeProvider>
+      </LocalizationProvider>
     </AuthProvider>
   );
 }
