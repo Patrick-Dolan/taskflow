cd33c1f5694510f461e69eae844e57c1b6d6e01f
Patrick Dolan <dolanp1992@gmail.com>
2023-01-26 13:27:00 -0800

    refactor: change register account textfields to be in a form and add error handling

--- src/Components/dialogs/UserAuthDialog.js
+++ src/Components/dialogs/UserAuthDialog.js
@@ -30,20 +30,63 @@ const UserAuthDialog = (props) => {
   const { signIn, registerUser } = UserAuth();
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+
+  // Register State
   const [registerEmail, setRegisterEmail] = useState("");
-  const [passwordRegister, setPasswordRegister] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerPasswordConfirmation, setRegisterPasswordConfirmation] = useState("");
+
+  // Shared Auth state
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+
+  const clearErrors = () => {
+    setEmailError(false);
+    setEmailErrorMessage("");
+    setPasswordError(false);
+    setPasswordErrorMessage("");
+  }
 
   const handleUserAuthDialogClose = () => {
+    clearErrors();
     setOpen(false);
   };
 
+  const passwordsDifferent = () => {
+    if (registerPassword !== registerPasswordConfirmation) {
+      setPasswordError(true);
+      setPasswordErrorMessage("Passwords do not match.");
+      return true;
+    }
+    setPasswordError(false);
+    return false;
+  }
+
   const handleRegisterAccount = async (e) => {
     e.preventDefault();
+    clearErrors();
+
+    if (passwordsDifferent()) {
+      return;
+    }
+
     try {
-      await registerUser(registerEmail, passwordRegister);
+      await registerUser(registerEmail, registerPassword);
       handleUserAuthDialogClose();
     } catch (e) {
-      console.log("Register User Error: ", e.message);
+      if (e.code === "auth/invalid-email") {
+        setEmailError(true);
+        setEmailErrorMessage("Invalid email address.");
+      }
+      if (e.code === "auth/weak-password") {
+        setPasswordError(true);
+        setPasswordErrorMessage("Password must be at least 6 characters long.")
+      } else {
+        console.log("Register User Error: ", e.code);
+        console.log("Register User Error: ", e.message);
+      }
     }
   }
 
@@ -62,39 +105,45 @@ const UserAuthDialog = (props) => {
       <Dialog open={open} onClose={handleUserAuthDialogClose}>
         <Typography variant="h4" sx={{textAlign: "center", padding: "1em 0 0 0"}}>Sign up</Typography>
         <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="outlined"
-            required
-            onChange={(e) => setRegisterEmail(e.target.value)}
+          <form onSubmit={handleRegisterAccount}>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="outlined"
+              error={emailError}
+              helperText={(emailError) ? `${emailErrorMessage}` : ""}
+              onChange={(e) => setRegisterEmail(e.target.value)}
+              />
+            <TextField
+              margin="dense"
+              label="Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+              error={passwordError}
+              required
+              onChange={(e) => setRegisterPassword(e.target.value)}
+              />
+            <TextField
+              margin="dense"
+              label="Confirm Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+              error={passwordError}
+              helperText={(passwordError) ? `${passwordErrorMessage}` : ""}
+              required
+              onChange={(e) => setRegisterPasswordConfirmation(e.target.value)}
             />
-          <TextField
-            margin="dense"
-            label="Password"
-            type="password"
-            fullWidth
-            variant="outlined"
-            required
-            onChange={(e) => setPasswordRegister(e.target.value)}
-          />
-          {/* TODO: Setup password confirmation */}
-          {/* <TextField
-            margin="dense"
-            label="Confirm Password"
-            type="password"
-            fullWidth
-            variant="outlined"
-            required
-          /> */}
-          <Box
-            sx={{margin: "1.5em 0"}}
-          >
-            <Button fullWidth size="large" onClick={handleRegisterAccount} variant="contained">Sign up</Button>
-          </Box>
+            <Box
+              sx={{margin: "1.5em 0"}}
+            >
+              <Button fullWidth type="submit" size="large" variant="contained">Sign up</Button>
+            </Box>
+          </form>
           <Typography sx={{color: "grey"}} variant="caption">Already have an account? </Typography>
           <Link style={{textDecorationColor: "grey", color: "grey"}}>
             <Typography sx={{color: "grey"}} variant="caption" onClick={toggleLoginSignup}>Sign up here!</Typography>
