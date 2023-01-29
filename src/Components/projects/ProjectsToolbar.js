265c37fab4cba1b01877ff92292abac54479da70
Patrick Dolan <dolanp1992@gmail.com>
2023-01-29 15:59:58 -0800

    refactor: make toolbar responsive for smaller screens     update: make search functionality a toggle with icon and create button an icon

--- src/Components/projects/ProjectsToolbar.js
+++ src/Components/projects/ProjectsToolbar.js
@@ -1,45 +1,84 @@
-import { Autocomplete, Box, Button, Divider, TextField, Typography } from "@mui/material";
+import { Autocomplete, Button, Divider, Grid, TextField, Tooltip, Typography } from "@mui/material";
+import { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import { useTheme } from "@emotion/react";
 
 const ProjectsToolbar = (props) => {
   const { handleCreateProjectDialogBox, projects, searchedProject, setSearchedProject } = props;
+  const [showSearch, setShowSearch] = useState(false);
+  const theme = useTheme();
 
   const projectNames = projects.map((e) => (e.name));
 
+  const buttonStyles = {
+    maxHeight: "2.5em",
+    minHeight: "2.5em",
+    maxWidth: "2.5em",
+    minWidth: "2.5em",
+  }
+
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: ".5em"
-        }}
+      <Grid 
+        container 
+        justifyContent="space-between"
+        alignItems="center"
       >
-        <Typography variant="h4">Projects</Typography>
-        <Box
+        <Grid item>
+          <Typography variant="h4">Projects</Typography>
+        </Grid>
+        <Grid item>
+          <Grid container spacing="4">
+            <Grid item>
+              <Tooltip title="Add Project" enterDelay={500} leaveDelay={200}>
+                <Button sx={buttonStyles} variant="contained" size="small" onClick={handleCreateProjectDialogBox}><AddIcon /></Button>
+              </Tooltip>
+            </Grid>
+            <Grid item>
+              <Tooltip title="Search Projects" enterDelay={500} leaveDelay={200}>
+                <Button 
+                  sx={{
+                    ...buttonStyles, 
+                    backgroundColor: (showSearch) ? theme.palette.primary.light : theme.palette.primary.main
+                  }} 
+                  variant="contained" 
+                  size="small" 
+                  onClick={() => setShowSearch(!showSearch)}
+                >
+                  <SearchIcon />
+                </Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider 
+        sx={{
+          marginBottom: ".65em"
+        }}
+      />
+      {(showSearch) ? 
+        <Autocomplete
           sx={{
-            display: "flex"
+            marginBottom: ".65em"
           }}
-        >
-          <Autocomplete
-            options={projectNames}
-            getOptionLabel={(option) => option}
-            size="small"
-            inputValue={searchedProject}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                // TODO setup mobile view
-                sx={{minWidth: { sm: "15em", md: "30em" }}}
-                label="Search for a project"
-                variant="outlined"
-              />
-            )}
-            onInputChange={(event, value) => setSearchedProject(value)}
-          />
-          <Button variant="outlined" onClick={handleCreateProjectDialogBox}>Create Project</Button>
-        </Box>
-      </Box>
-      <Divider />
+          options={projectNames}
+          getOptionLabel={(option) => option}
+          size="small"
+          inputValue={searchedProject}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              sx={{minWidth: { sm: "15em", md: "30em" }}}
+              label="Search for a project"
+              variant="outlined"
+            />
+          )}
+          onInputChange={(event, value) => setSearchedProject(value)}
+        />
+        : null
+      }
     </>
   )
 }
--- src/Components/tasks/TasksToolbar.js
+++ src/Components/tasks/TasksToolbar.js
@@ -1,45 +1,84 @@
-import { Autocomplete, Box, Button, Divider, TextField, Typography } from "@mui/material";
+import { Autocomplete, Box, Button, Divider, Grid, TextField, Tooltip, Typography } from "@mui/material";
+import { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import { useTheme } from "@emotion/react";
 
 const TasksToolbar = (props) => {
   const { tasks, handleTaskCreateOpen, searchedTask, setSearchedTask } = props;
+  const [showSearch, setShowSearch] = useState(false);
+  const theme = useTheme();
+
   const taskNames = tasks.map((e) => (e.name));
+
+  const buttonStyles = {
+    maxHeight: "2.5em",
+    minHeight: "2.5em",
+    maxWidth: "2.5em",
+    minWidth: "2.5em",
+  }
 
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: ".5em"
-        }}
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
       >
-        <Typography variant="h4">Tasks</Typography>
-        <Box
+        <Grid item>
+          <Typography variant="h4">Tasks</Typography>
+        </Grid>
+        <Grid item>
+          <Grid container spacing="4">
+          <Grid item>
+              <Tooltip title="Add Task" enterDelay={500} leaveDelay={200}>
+                <Button sx={buttonStyles} variant="contained" size="small" onClick={handleTaskCreateOpen}><AddIcon /></Button>
+              </Tooltip>
+            </Grid>
+            <Grid item>
+              <Tooltip title="Search Tasks" enterDelay={500} leaveDelay={200}>
+                <Button 
+                  sx={{
+                    ...buttonStyles, 
+                    backgroundColor: (showSearch) ? theme.palette.primary.light : theme.palette.primary.main
+                  }} 
+                  variant="contained" 
+                  size="small" 
+                  onClick={() => setShowSearch(!showSearch)}
+                >
+                  <SearchIcon />
+                </Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider 
+        sx={{
+          marginBottom: ".65em"
+        }}
+      />
+      {(showSearch) ?
+        <Autocomplete
           sx={{
-            display: "flex",
-            alignItems: "center"
+            marginBottom: ".65em"
           }}
-        >
-          <Autocomplete
-            options={taskNames}
-            getOptionLabel={(option) => option}
-            inputValue={searchedTask || ""}
-            size="small"
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                // TODO setup mobile view
-                sx={{minWidth: { sm: "15em", md: "30em" }}}
-                label="Search for a task"
-                variant="outlined"
-              />
-            )}
-            onInputChange={(event, value) => setSearchedTask(value)}
-          />
-          <Button variant="outlined" size="large" onClick={handleTaskCreateOpen}>Create task</Button>
-        </Box>
-      </Box>
-      <Divider />
+          options={taskNames}
+          getOptionLabel={(option) => option}
+          inputValue={searchedTask || ""}
+          size="small"
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              sx={{minWidth: { sm: "15em", md: "30em" }}}
+              label="Search for a task"
+              variant="outlined"
+            />
+          )}
+          onInputChange={(event, value) => setSearchedTask(value)}
+        />
+        : null
+      }
     </>
   )
 }
