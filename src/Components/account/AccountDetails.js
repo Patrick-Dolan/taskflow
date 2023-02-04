ce64bcb579698a9b364a9903753e07d4eb9ced35
Patrick Dolan <dolanp1992@gmail.com>
2023-02-04 15:07:56 -0800

    refactor: change how information is displayed and add profile information section

--- src/Components/account/AccountDetails.js
+++ src/Components/account/AccountDetails.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Grid, Typography, Divider } from "@mui/material";
 import { Box } from "@mui/system";
 
 
@@ -7,26 +7,83 @@ const AccountDetails = (props) => {
 
   return (
     <>
+      <Divider><Typography variant="h4">My Account</Typography></Divider>
       <Box
         sx={{
           display: "flex",
-          justifyContent: "space-between"
+          justifyContent: "space-between",
+          marginTop: "1em",
+          marginBottom: ".25em"
         }}
       >
-        <Typography variant="h3">{user.firstName ? user.firstName : user.displayName}</Typography>
+        <Typography variant="h5">Account information</Typography>
         <Button 
           onClick={handleAccountEditClick}
           size="small"
+          variant="outlined"
         >Edit account</Button>
       </Box>
-      <Typography variant="subtitle2">Username:</Typography>
-      <Typography variant="body1">{user.displayName}</Typography>
-      <Typography variant="subtitle2">Email:</Typography>
-      <Typography variant="body1">{user.email}</Typography>
-      <Typography variant="subtitle2">First name:</Typography>
-      <Typography variant="body1">{user.firstName ? user.firstName : "No first name available."}</Typography>
-      <Typography variant="subtitle2">Last name:</Typography>
-      <Typography variant="body1">{user.lastName ? user.lastName : "No last name available."}</Typography>
+      <Divider sx={{marginBottom: "1em"}} />
+      <Grid container direction="column" spacing={3}>
+        <Grid container item>
+          <Grid item xs={12} sm={3} md={3} lg={3}>
+            <Typography variant="subtitle2">Username:</Typography>
+          </Grid>
+          <Grid item xs={12} sm={9} md={9} lg={9}>
+            <Typography variant="body1">{user.displayName}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container item>
+          <Grid item xs={12} sm={3} md={3} lg={3}>
+            <Typography variant="subtitle2">Email:</Typography>
+          </Grid>
+          <Grid item xs={12} sm={9} md={9} lg={9}>
+            <Typography variant="body1">{user.email}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container item>
+          <Grid item xs={12} sm={3} md={3} lg={3}>
+            <Typography variant="subtitle2">Password:</Typography>
+          </Grid>
+          <Grid item xs={12} sm={9} md={9} lg={9}>
+            <Typography variant="body1">{user.password ? user.password : "No first name available."}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "2em",
+          marginBottom: ".25em"
+        }}
+      >
+        <Typography variant="h5">Profile information</Typography>
+        <Button 
+          onClick={handleAccountEditClick}
+          size="small"
+          variant="outlined"
+        >Edit profile</Button>
+      </Box>
+      <Divider sx={{marginBottom: "1em"}} />
+      <Grid container direction="column" spacing={3}>
+        <Grid container item>
+          <Grid item xs={12} sm={3} md={3} lg={3}>
+            <Typography variant="subtitle2">First name:</Typography>
+          </Grid>
+          <Grid item xs={12} sm={9} md={9} lg={9}>
+            <Typography variant="body1">{user.firstName ? user.firstName : "No first name available."}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container item>
+          <Grid item xs={12} sm={3} md={3} lg={3}>
+            <Typography variant="subtitle2">Last name:</Typography>
+          </Grid>
+          <Grid item xs={12} sm={9} md={9} lg={9}>
+            <Typography variant="body1">{user.lastName ? user.lastName : "No last name available."}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   )
 }
