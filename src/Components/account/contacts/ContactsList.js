177d682974268e1d6f0a8b7a1438c991ec78ea9c
Patrick Dolan <dolanp1992@gmail.com>
2023-02-12 14:50:07 -0800

    update: add search contacts sorting to contacts list

--- src/Components/account/contacts/ContactsList.js
+++ src/Components/account/contacts/ContactsList.js
@@ -1,24 +1,27 @@
 import { Avatar, List, ListItemAvatar, ListItemButton, ListItemText } from "@mui/material";
 
 const ContactsList = (props) => {
-  const { contacts, selectContact } = props;
+  const { contacts, selectContact, searchedContact } = props;
 
   return(
     <> 
       <List>
-      {contacts.map((contact) => (
-        <ListItemButton
-          key={contact.uid}
-          onClick={() => selectContact(contact)}
-        >
-          <ListItemAvatar>
-            <Avatar alt={contact.displayName} src={contact.photoURL} />
-          </ListItemAvatar>
-          <ListItemText 
-            primary={contact.displayName}
-            secondary={contact.email}
-          />
-        </ListItemButton>
+      {(searchedContact
+          ? contacts?.sort((a, b) => a.displayName === searchedContact ? -1 : b.displayName === searchedContact ? 1 : 0)
+          : contacts?.sort((a, b) => a.displayName.localeCompare(b.displayName))
+        ).map((contact) => (
+          <ListItemButton
+            key={contact.uid}
+            onClick={() => selectContact(contact)}
+          >
+            <ListItemAvatar>
+              <Avatar alt={contact.displayName} src={contact.photoURL} />
+            </ListItemAvatar>
+            <ListItemText 
+              primary={contact.displayName}
+              secondary={contact.email}
+            />
+          </ListItemButton>
       ))}
       </List>
     </>
