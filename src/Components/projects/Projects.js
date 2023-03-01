e960238f531424e1e4baab8d437ff9e68194648a
Patrick Dolan <dolanp1992@gmail.com>
2023-03-01 12:52:13 -0800

    refactor: comment out dummy data in projects and add useEffect that fetchs project data from firestore for display

--- src/Components/projects/Projects.js
+++ src/Components/projects/Projects.js
@@ -1,58 +1,81 @@
 import { Container } from "@mui/system";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProjectCreateDialog from "./ProjectCreateDialog";
 import ProjectsToolbar from "./ProjectsToolbar";
 import ProjectsList from "./ProjectsList";
 import ProjectDetails from "./ProjectDetails";
 import { v4 as uuid } from "uuid";
+import { getProjects } from "../../FirebaseFunctions";
+import { UserAuth } from "../../Contexts/AuthContext";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([
-    {
-      id: uuid(),
-      name: "TaskFlow", 
-      description: "A project management application that incorporates both task assignments and communication features.",
-      tasks: [
-        "Build project functionality",
-        "Build task functionality",
-        "Setup firestore database",
-        "Setup Hosting through firebase",
-        "Add to portfolio"
-      ]
-    },
-    {
-      id: uuid(),
-      name: "Build a Custom PC",
-      description: "Assemble a custom built PC tailored to your gaming or work needs, with the latest hardware and design.",
-      tasks: [
-        "Research and select the components for the build (CPU, GPU, motherboard, etc.)",
-        "Acquire the necessary tools for the build (screwdrivers, thermal paste, etc.)",
-        "Assemble the PC according to the chosen design",
-        "Install the operating system and necessary drivers",
-        "Test and troubleshoot the build to ensure it is fully functional"
-      ]
-    },
-    {
-      id: uuid(),
-      name: "Cosplay Creation",
-      description: "Create a detailed cosplay of a favorite character from a geek culture series, movie, or game.",
-    },
-    {
-      id: uuid(),
-      name: "Tabletop RPG Campaign",
-      description: "Create and run a campaign for a group of friends to play through a tabletop roleplaying game.",
-      tasks: [
-        "Research and select a tabletop RPG system to use for the campaign",
-        "Create a detailed world and lore for the campaign",
-        "Design and create non-player characters and enemies for the campaign",
-        "Write and prepare a series of quests and events for the players to experience",
-        "Run and facilitate the campaign sessions with the group"
-      ]
-    },
-  ]);
+  const { user } = UserAuth();
+  const [projects, setProjects] = useState(
+    [
+      // {
+      //   id: uuid(),
+      //   name: "TaskFlow", 
+      //   description: "A project management application that incorporates both task assignments and communication features.",
+      //   tasks: [
+      //     "Build project functionality",
+      //     "Build task functionality",
+      //     "Setup firestore database",
+      //     "Setup Hosting through firebase",
+      //     "Add to portfolio"
+      //   ]
+      // },
+      // {
+      //   id: uuid(),
+      //   name: "Build a Custom PC",
+      //   description: "Assemble a custom built PC tailored to your gaming or work needs, with the latest hardware and design.",
+      //   tasks: [
+      //     "Research and select the components for the build (CPU, GPU, motherboard, etc.)",
+      //     "Acquire the necessary tools for the build (screwdrivers, thermal paste, etc.)",
+      //     "Assemble the PC according to the chosen design",
+      //     "Install the operating system and necessary drivers",
+      //     "Test and troubleshoot the build to ensure it is fully functional"
+      //   ]
+      // },
+      // {
+      //   id: uuid(),
+      //   name: "Cosplay Creation",
+      //   description: "Create a detailed cosplay of a favorite character from a geek culture series, movie, or game.",
+      // },
+      // {
+      //   id: uuid(),
+      //   name: "Tabletop RPG Campaign",
+      //   description: "Create and run a campaign for a group of friends to play through a tabletop roleplaying game.",
+      //   tasks: [
+      //     "Research and select a tabletop RPG system to use for the campaign",
+      //     "Create a detailed world and lore for the campaign",
+      //     "Design and create non-player characters and enemies for the campaign",
+      //     "Write and prepare a series of quests and events for the players to experience",
+      //     "Run and facilitate the campaign sessions with the group"
+      //   ]
+      // }
+    ]
+  );
+  const [refreshProjects, setRefreshProjects] = useState(false);
   const [selectedProject, setSelectedProject] = useState({});
   const [searchedProject, setSearchedProject] = useState("");
   const [openProjectCreate, setOpenProjectCreate] = useState(false);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const projects = await getProjects(user.uid);
+        console.log(projects)
+        setProjects([
+          ...projects
+        ]) 
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+
+    fetchProjects();
+
+  }, [refreshProjects, user.uid])
 
   const handleProjectCreateOpen = () => {
     setOpenProjectCreate(true);
@@ -84,9 +107,9 @@ const Projects = () => {
       <ProjectCreateDialog
         open={openProjectCreate}
         setOpen={setOpenProjectCreate}
-        projects={projects}
-        setProjects={setProjects}
         setSelectedProject={setSelectedProject}
+        refreshProjects={refreshProjects}
+        setRefreshProjects={setRefreshProjects}
       />
     </Container>
   )
