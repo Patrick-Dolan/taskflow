4cdd5624afeca03fe535bcf9c4c7e017cd7f3273
Patrick Dolan <dolanp1992@gmail.com>
2023-03-03 12:18:39 -0800

    update: add success and error toast to notify user about password recovery operation     refactor: consolidate imports to use less code

--- src/Components/account/PasswordRecovery.js
+++ src/Components/account/PasswordRecovery.js
@@ -1,15 +1,17 @@
-import { Container } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Container, Box } from "@mui/system";
+import { Typography, Snackbar, Alert, Button, TextField } from "@mui/material";
 import { UserAuth } from "../../Contexts/AuthContext";
 import { useState } from "react";
-import TextField from '@mui/material/TextField';
-import { Box } from "@mui/system";
-import { Button } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 
 const PasswordRecovery = () => {
+  const theme = useTheme();
   const { passwordResetEmail } = UserAuth();
   const [passwordEmail, setPasswordEmail] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarAlertSeverity, setSnackbarAlertSeverity] = useState("success")
 
 
   const handlePasswordReset = async (e) => {
@@ -17,17 +19,22 @@ const PasswordRecovery = () => {
 
     try {
       await passwordResetEmail(passwordEmail);
-      // TODO set up snackbar for password reset
-      console.log("Password Reset Email Sent");
+
+      // Notify User of success or error of passwordResetEmail
+      setSnackbarAlertSeverity("success")
+      setSnackbarMessage("Password reset email sent successfully.")
+      setSnackbarOpen(true);
     } catch (e) {
-      console.log(e.message);
+      setSnackbarAlertSeverity("error")
+      setSnackbarMessage(`Password reset email error: ${e.message}`)
+      setSnackbarOpen(true);
     }
   }
 
   return (
     <Container maxWidth="md">
       <Box>
-        <Typography variant="h4">Password Recovery placeholder</Typography>
+        <Typography variant="h4">Password Recovery</Typography>
         <TextField
           autoFocus
           margin="dense"
@@ -40,6 +47,20 @@ const PasswordRecovery = () => {
         />
         <Button fullWidth size="large" onClick={handlePasswordReset} variant="contained">Send password reset email</Button>
       </Box>
+      <Snackbar
+        open={snackbarOpen} 
+        autoHideDuration={3000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert 
+          severity={snackbarAlertSeverity} 
+          sx={{
+          backgroundColor: (snackbarAlertSeverity === "success") ? theme.palette.success.light : theme.palette.error.light
+          }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Container>
   )
 }
